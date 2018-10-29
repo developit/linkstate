ff5007aaf7797942e6a5bdc8baae9c498c0f1095
Justin Bennett <zephraph@gmail.com>
2018-10-28 20:54:24 -0400

    Allow key to be an array
    I ran into a usecase where the path to a state value had a string that contained a `.`. Specifically a URL.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import delve from 'dlv';
 
 /** Create an Event handler function that sets a given state property.
  *	@param {Component} component	The component whose state should be updated
- *	@param {string} key				A dot-notated key path to update in the component's state
- *	@param {string} eventPath		A dot-notated key path to the value that should be retrieved from the Event or component
+ *	@param {string|string[]} key	A dot-notated key path to update in the component's state
+ *	@param {string} eventPath	A dot-notated key path to the value that should be retrieved from the Event or component
  *	@returns {function} linkedStateHandler
  */
 export default function linkState(component, key, eventPath) {
-	let path = key.split('.'),
+	let path = key instanceof Array ? key : key.split('.'),
 		cache = component.__lsc || (component.__lsc = {});
 
 	return cache[key+eventPath] || (cache[key+eventPath] = function(e) {
